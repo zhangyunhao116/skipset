a9b70d3525c27aa725713f6751113d2f9be53fde
ZYunH <zyunhjob@163.com>
2020-10-10 00:28:33 +0800

    use fastrand

--- bench.sh
+++ bench.sh
@@ -1,9 +1,10 @@
 # benchstat installation: go get -u golang.org/x/perf/cmd/benchstat
 
 # 100,000 operations.
-go test -bench=. -test.run=NOTEST -timeout=30m -benchmem -cpu=8 -count=100 -benchtime=100000x  > 100,000-100.txt
-benchstat 100,000-100.txt
+go test -bench=. -run=NOTEST -benchmem -cpu=8 -count=20 -benchtime=100000x  > 10t.txt
 
 # 1,000,000 operations.
-#go test -bench=. -test.run=NOTEST -timeout=30m -benchmem -cpu=8 -count=100 -benchtime=1000000x > 1,000,000-100.txt
+go test -bench=. -run=NOTEST -benchmem -cpu=8 -count=20 -benchtime=1000000x > 100t.txt
+
 #benchstat 1,000,000-100.txt
+#benchstat 100,000-100.txt
--- config.go
+++ config.go
@@ -1,7 +1,7 @@
 package skipset
 
 import (
-	"math/rand"
+	_ "unsafe"
 )
 
 const (
@@ -9,9 +9,19 @@ const (
 	p        = 0.25
 )
 
+//go:linkname runtime_fastrand runtime.fastrand
+func runtime_fastrand() uint32
+
+//go:nosplit
+func fastrandn(n uint32) uint32 {
+	// This is similar to fastrand() % n, but faster.
+	// See https://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
+	return uint32(uint64(runtime_fastrand()) * uint64(n) >> 32)
+}
+
 func randomLevel() int {
 	level := 1
-	for rand.Float64() < p {
+	for fastrandn(1/p) == 0 {
 		level++
 	}
 	if level > maxLevel {

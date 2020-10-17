2a2c05b4860f9579da03a1e8537fc3029d8eb248
ZhangYunHao <zyunhjob@163.com>
2020-10-18 02:16:07 +0800

    refactor project structure (#2)
    rm debug.go && doc.go and mv config.go to rand.go

--- doc.go
+++
@@ -1,4 +0,0 @@
-// Package skipset is a high-performance concurrent set based on skip list.
-// In typical pattern(one million operations, 90%CONTAINS 9%INSERT 1%DELETE),
-// the skipset up to 3x ~ 6x faster than the built-in sync.Map.
-package skipset

8629104572abbe262e7255febcbc517e75724cc5
ZYunH <zyunhjob@163.com>
2020-10-18 01:49:10 +0800

    remove doc.go

--- skipset.go
+++ skipset.go
@@ -1,3 +1,6 @@
+// Package skipset is a high-performance concurrent set based on skip list.
+// In typical pattern(one million operations, 90%CONTAINS 9%INSERT 1%DELETE),
+// the skipset up to 3x ~ 10x faster than the built-in sync.Map.
 package skipset
 
 import (
